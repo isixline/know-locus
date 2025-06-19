221c63f482eaa92676fb9522ebc20c8a8fdc2980
isixline <isixline@qq.com>
2025-06-19 14:37:17 +0800

    refactor: use lru_cache get model

--- src/utils/vectorizer.py
+++ src/utils/vectorizer.py
@@ -1,18 +1,18 @@
 from sentence_transformers import SentenceTransformer
 from utils.timers import track_time
+from functools import lru_cache
 
-# 加载预训练模型（推荐这个：小巧，速度快，效果不错）
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
+@lru_cache(maxsize=2)
+def get_model(name="all-MiniLM-L6-v2"):
+    return SentenceTransformer(name)
 
 @track_time
 def vectorized_corpus(get_corpus: callable):
-    corpus_vector = model.encode(get_corpus(), batch_size=64, show_progress_bar=True)
+    corpus_vector = get_model().encode(get_corpus(), batch_size=64, show_progress_bar=True)
     return corpus_vector
 
 
 @track_time
-def vectorize_text(query: str):
-    query_vector = model.encode(query)
-
-    return query_vector
+def vectorize_text(text: str):
+    text_vector = get_model().encode(text)
+    return text_vector
